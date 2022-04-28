76757f4808215907b0a54283f98e8f5b4dcd088e
Yan Sun <tsun812@gmail.com>
2022-04-28 16:43:29 -0700

    allow cors on server

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.use(cors());
+
 const User = require("./database/models").User;
 const port = 3000;
 
