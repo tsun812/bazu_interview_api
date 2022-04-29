466f434cc45e31150b553a4024a20945231a1c6a
Yan Sun <tsun812@gmail.com>
2022-04-29 12:08:48 -0700

    add cors, finish put request handling, milestone 1

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const bodyparser = require("body-parser");
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded());
-
-app.use(cors());
-
+//app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+//app.use(cors());
 const User = require("./database/models").User;
 const port = 3000;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://192.168.0.100:19003");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.get("/users/:id", (req, res) => {
   let id = req.params.id;
   User.findAll({
@@ -18,18 +26,19 @@ app.get("/users/:id", (req, res) => {
       let promo = user[0].dataValues.promo;
       res.json({ promo: promo });
     })
-    .catch((err) => res.send(404));
+    .catch((err) => res.send(err));
 });
 
-app.post("/users/:id", (req, res) => {
+app.put("/users/:id", (req, res) => {
   let id = req.params.id;
   let newPromo = req.body.newPromo;
-  User.update({ promo: newPromo }, { where: id })
-    .then((res) => {
-      User.save();
+  let promo = "asdf";
+  console.log(req.body);
+  User.update({ promo: newPromo }, { where: { id: id } })
+    .then((response) => {
       res.send(200);
     })
-    .catch((err) => res.send(404));
+    .catch((err) => res.send(err));
 });
 
 app.listen(port, () => {
